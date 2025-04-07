ec85ea6b83569d04190b1aac1e4fab7501abc035
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-04-07 20:17:03 +0700

    feat: add '#build' method to make balanced tree

--- src/Tree.js
+++ src/Tree.js
@@ -1,9 +1,33 @@
-import Node from "./src/Node.js";
+import Node from "./Node.js";
 
-export class Tree {
+export default class Tree {
 	#root;
 
-	constructor() {
-		this.#root = null;
+	constructor(array) {
+		this.#root = this.#build(array);
+	}
+
+	get root() {
+		return this.#root;
+	}
+
+	#build(array) {
+		const values = [...new Set(array)].sort((a, b) => a - b);
+		const start = 0;
+		const end = values.length - 1;
+
+		return this.#split(values, start, end);
+	}
+
+	#split(values, start, end) {
+		if (start > end) return null;
+
+		const mid = start + Math.floor((end - start) / 2);
+		const node = new Node(values[mid]);
+
+		node.left = this.#split(values, start, mid - 1);
+		node.right = this.#split(values, mid + 1, end);
+
+		return node;
 	}
 }
--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-import Tree from "./src/Tree.js";
+import Tree from "./Tree.js";
+
+const array = [4, 3, 2, 1, 5, 6, 7];
+const tree = new Tree(array);
+
+console.log(tree.root.value);
